9d59d4beda085779b52b4e18f2ccacf88d64a1b6
Ubuntu <ubuntu@ip-172-31-51-232.ec2.internal>
2021-10-05 02:34:08 +0000

    added file

--- guessing_game2.cpp
+++ guessing_game2.cpp
@@ -22,25 +22,26 @@ int main() {
 
     // input
     while (true) {
-        std::cout << "Enter a random number between 0 and 50: ";
+        std::cout << "Enter a random number between 0 and 10(integer): ";
         std::cin >> random_guess;
         std::cout << "" << std::endl;
 
         try {
             random_guess_as_integer = std::stoi(random_guess);
             if (random_guess_as_integer == the_number) {
-                std::cout << "Correct, good guesser."
-                            << random_guess << std::endl;
+                std::cout << "Correct, good guesser." << std::endl;
+                std::cout << "\nDone." << std::endl;
+                break;
             } else if (random_guess_as_integer > the_number) {
                 std::cout << "The number is too high, try again." << std::endl;
             } else {
                 std::cout << "The number is too low, try again." << std::endl;
+            }
         }
-    }
-    catch (std::invalid_argument) {
+        catch (std::invalid_argument) {
         std::cout << random_guess << " is an invalid input, try again."
                     << std::endl;
-    }
+        }
     std::cout << "\nDone." << std::endl;
     }
 }
